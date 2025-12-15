445fe112edbcb9a8308b8beab13c209e43ebd6f3
keisha De La Rosa <keishdlr@gmail.com>
2025-12-15 11:49:45 -0500

    add appropiate annotation GET action for getbyID, return category by ID

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -37,12 +37,12 @@ public class CategoriesController
         return categoryDao.getAllCategories();
     }
 
-    // add the appropriate annotation for a get action
+    // ✅add the appropriate annotation for a get action
     @GetMapping
     public Category getById(@PathVariable int id)
     {
-        // get the category by id
-        return null;
+        // ✅get the category by id
+        return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
