9ad7d020d485a0130da5e4c8acfafc43ac8deb0d
keisha De La Rosa <keishdlr@gmail.com>
2025-12-15 11:31:40 -0500

    add REST controller annotations, add annotation for get all method in CategoriesController

--- src/main/java/org/yearup/EasyshopApplication.java
+++ src/main/java/org/yearup/EasyshopApplication.java
@@ -8,7 +8,15 @@ public class EasyshopApplication
 {
 
     public static void main(String[] args) {
-        SpringApplication.run(EasyshopApplication.class, args);
+
+        if(args.length != 2){
+            System.exit(0);
+        }else{
+            System.setProperty("dbUsername", args[0]);
+            System.setProperty("dbPassword", args[1]);
+            SpringApplication.run(EasyshopApplication.class, args);
+        }
+
     }
 
 }
--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -3,6 +3,7 @@ package org.yearup.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -10,10 +11,11 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
+// ✅add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+@RestController
 public class CategoriesController
 {
     private CategoryDao categoryDao;
@@ -22,7 +24,8 @@ public class CategoriesController
 
     // create an Autowired controller to inject the categoryDao and ProductDao
 
-    // add the appropriate annotation for a get action
+    //✅ add the appropriate annotation for a get action
+    @GetMapping("/api/records")
     public List<Category> getAll()
     {
         // find and return all categories
