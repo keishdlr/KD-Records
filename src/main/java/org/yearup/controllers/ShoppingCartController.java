ebd226085c8487b5ea285b3721251f8005558c0e
keisha De La Rosa <keishdlr@gmail.com>
2025-12-18 11:18:42 -0500

    convert class to a REST Controller

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,6 +1,10 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -10,15 +14,15 @@ import org.yearup.models.User;
 
 import java.security.Principal;
 
-// convert this class to a REST controller
+// ✅convert this class to a REST controller
 // only logged in users should have access to these actions
+@RestController
 public class ShoppingCartController
 {
     // a shopping cart requires
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
-
 
 
     // each method in this controller requires a Principal object as a parameter
@@ -43,14 +47,29 @@ public class ShoppingCartController
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    @PostMapping
+    public ShoppingCart addToCart(){
+
+        return ShoppingCart;
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
+    @PutMapping
+    public ShoppingCart update(){
+
+        return ShoppingCart;
+    }
 
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart
+    @DeleteMapping
+    public ShoppingCart delete(){
+
+        return ShoppingCart;
+    }
 
 }
